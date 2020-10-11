83f1fc8fd4bb7c9f44b29efdf247e58cd6dc971e
Leonardo Antunes <leonoman07@gmail.com>
2020-10-11 06:04:03 -0300

    Cotinuação até aula 19.

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -5,14 +5,27 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import NumeroAleatorio from './components/basicos/NumeroAleatorio'
+import Card from './components/layout/Card'
 
 export default () =>
     <div id="App">
         <h1>Fundamentos React</h1>
 
+        <Card titulo="#04 - Desafio Aleatório">
         <NumeroAleatorio valorMinimo={1} valorMaximo={1000} />
-        <Fragmento />
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+        <Fragmento/>
+        </Card>
+
+        <Card titulo="#02 - Com Parâmetro">
         <ComParametro aluno="Pedro" nota={9.3} />
+        </Card>
+
+        <Card titulo="#01 - Primeiro">        
         <Primeiro />
+        </Card>
+
 
     </div>
