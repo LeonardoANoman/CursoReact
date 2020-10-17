224caa39ff00a073294221d09b9f42182e403016
Leonardo Antunes <leonoman07@gmail.com>
2020-10-17 14:46:44 -0300

    Aula 45.

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -4,7 +4,17 @@ import "./Calculator.css";
 import Button from "../components/Button";
 import Display from "../components/Display";
 
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 class Calculator extends Component {
+  state = { ...initialState };
+
   constructor(props) {
     super(props);
 
@@ -12,16 +22,37 @@ class Calculator extends Component {
     this.setOperation = this.setOperation.bind(this);
     this.addDigit = this.addDigit.bind(this);
   }
-  clearMemory() {}
+  clearMemory() {
+    this.setState({ ...initialState });
+  }
 
   setOperation(operation) {}
 
-  addDigit(n) {}
+  addDigit(n) {
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
+      console.log(values);
+    }
+  }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={100}></Display>
+        <Display value={this.state.displayValue}></Display>
         <Button label={"AC"} click={this.clearMemory} triple></Button>
         <Button label={"/"} click={this.setOperation} operation></Button>
         <Button label={"7"} click={this.addDigit}></Button>
