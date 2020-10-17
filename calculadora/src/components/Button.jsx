188c150c896aa280e2b253d89fd170b8e5c77392
Leonardo Antunes <leonoman07@gmail.com>
2020-10-17 10:02:04 -0300

    Aula 44.

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import "./Button.css";
 
 export default (props) => {
+  let classes = "button ";
+  classes += props.operation ? "operation" : "";
+  classes += props.double ? "double" : "";
+  classes += props.triple ? "triple" : "";
+
   return (
-    <div>
-      <button className="button">{props.label}</button>
-    </div>
+    <button
+      onClick={(e) => props.click && props.click(props.label)}
+      className={classes}
+    >
+      {props.label}
+    </button>
   );
 };
