fdebb971394e067b47199709c088bf80b76eb401
Leonardo Antunes <leonoman07@gmail.com>
2020-10-19 18:33:51 -0300

    Aula 63.

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default (props) => {
   return (
     <div>
-      <h1>Sobre</h1>
+      <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+      <h2>Nossa História</h2>
+      <p>Tá cheio de Glória</p>
+      <h2>Missão e Visão</h2>
+      <p>Fica ligado meu irmão</p>
+      <h2>Imprensa</h2>
+      <p>Cuidado.</p>
     </div>
   );
 };
