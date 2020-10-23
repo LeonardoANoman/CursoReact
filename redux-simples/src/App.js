2adc979b8e2833d86763592bc74e165a205f9fc6
Leonardo Antunes <leonoman07@gmail.com>
2020-10-23 19:30:50 -0300

    Aula 81.

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -6,7 +6,12 @@ function App() {
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
        <div className="linha">
-           <Card></Card>
+           <Card title="Card 1" red>X</Card>
+           <Card title="Card 2" green>X</Card>
+         </div>
+       <div className="linha">
+           <Card title="Card 2" blue>Y</Card>
+           <Card title="Card 2" purple>Y</Card>
          </div>
     </div>
   );
