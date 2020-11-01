b0fdebde5bdfec06751a72971420b40b622e3df7
Leonardo Antunes <leonoman07@gmail.com>
2020-11-01 19:40:21 -0300

    Aula 117.

---
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,7 @@
+const { model } = require('mongoose')
+const BillingCycle = require('./billingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true, runValidators: true})
+
+module.exports = BillingCycle
