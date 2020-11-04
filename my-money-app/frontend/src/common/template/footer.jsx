01ac80703a336f502e09394b5624caafc1c87215
Leonardo Antunes <leonoman07@gmail.com>
2020-11-04 14:56:57 -0300

    Aula 137.

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2017
+      <a href="http://leozinho.com.br" target="_blank">
+        Leo
+      </a>
+    </strong>
+  </footer>
+);
